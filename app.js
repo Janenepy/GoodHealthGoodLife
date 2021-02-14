dfa656aa983ca484c94ec18c4a840b7eec0c697a
Janenepy <77452820+Janenepy@users.noreply.github.com>
2021-02-14 21:01:11 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var assessmentRouter = require('./routes/assessment');
+var newsRouter = require('./routes/news');
+var resultRouter = require('./routes/result');
+var formRouter = require('./routes/form');
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/assessment', assessmentRouter);
+app.use('/news', newsRouter);
+app.use('/result', resultRouter);
+app.use('/form', formRouter);
+
+var server = app.listen(3000, function() {
+  console.log('Express is running on port 3000.');
+});
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
